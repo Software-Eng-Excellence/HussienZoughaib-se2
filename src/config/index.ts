018b0cc888f7577c1448be346b28af3931b26455
Hussiendev <hussienzoughaib@gamil.com>
2025-09-03 12:00:59 +0300

    git commit 'a';

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import dotnev from "dotenv";
 import path from "path";
-dotnev.config({path:path.join(__dirname,'../../.env')});
+dotnev.config({path:path.join(__dirname,'../ ../.env')});
 export default{
 
     secret:process.env.secret || 'secret'
